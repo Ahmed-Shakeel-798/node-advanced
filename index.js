cd3def917e7c5dd611780d767a27a411a9ee3573
Ahmed-Shakeel-798 <ahmed.shakeel.work@gmail.com>
2022-07-08 03:09:06 +0500

    clustering and benchmarking

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require("cluster");
 
 if(cluster.isMaster){
     // I am the master/cluster manager
-    console.log(`Am I master? ${cluster.isMaster}.`);
+    console.log(`Am I master? ${cluster.isMaster}.\nMy thread size is: ${process.env.UV_THREADPOOL_SIZE}`);
 
     cluster.fork(); // causes index.js to be executed again but in child mode
     cluster.fork();
     cluster.fork();
-    cluster.fork();
+    // cluster.fork();
 }else{
     // I am a child, and my job is to run the server
-    console.log(`Am I master? ${cluster.isMaster}.`);
+    console.log(`Am I master? ${cluster.isMaster}.\nMy thread size is: ${process.env.UV_THREADPOOL_SIZE}`);
 
     const express = require("express");
+    const crypto = require('crypto');
+
     const app = express();
 
-    function doWork(duration){
-        const start = Date.now();
-        while(Date.now() - start < duration){}
-    }
     
     app.get('/', (req, res) => {
-        doWork(5000);
-        res.send("Hi there!!!");
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send("Hi there!!!");
+        })
+        
     });
     
     app.listen(3000);
